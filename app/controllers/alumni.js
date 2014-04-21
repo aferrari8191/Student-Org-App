63cde9c33ff51a8dcb529e67da5ec69264ce07bf
cwstuart <cwstuart@uwm.edu>
2014-04-21 13:45:52 -0500

    notes from class. a little broken but can comment stuff out. trying to add onClick stuff for alumni/news tabs.

--- app/controllers/alumni.js
+++ app/controllers/alumni.js
@@ -6,6 +6,21 @@
  */
 var APP = require("core");
 //TODO: let alumni be onClick and bring up details on each w/ view. 
+//TODO: change views etc into TableViews... should work better for listing students, etc. 
+//TODO: check out quinn's cloud example from 4-21 to get info about array, looping etc pushing stuff to tableview
+// HASCHILD!!!!!!
+
+//$.myStuff.addEventListener("click", function(e)){
+//APP.addChild("text")	
+//};
+var infoWindow = Ti.UI.createView({
+	backgroundColor: 'white',
+	width: '100dp',
+	height: '100dp'
+});
+infoWindow.addEventListener('click', showInfo);
+add(infoWindow);
+
 var CONFIG = arguments[0];
 
 APP.log("debug", "text | " + JSON.stringify(CONFIG));
--- app/controllers/newsevents.js
+++ app/controllers/newsevents.js
@@ -8,6 +8,61 @@ var APP = require("core");
 //TODO: create onClick events for different tabs & news items for newsevents tab
 //TODO: create new views for those different items
 var Cloud = require("ti.cloud");
+var init = function() {
+	//loginUser();
+	
+	$.index.open();
+};
+
+var loginUser = function() {
+Cloud.Users.login({
+    login: 'test@test.com',
+    password: 'test_password'
+}, function (e) {
+    if (e.success) {
+        var user = e.users[0];
+        alert('Success:\n' +
+            'id: ' + user.id + '\n' +
+            'sessionId: ' + Cloud.sessionId + '\n' +
+            'first name: ' + user.first_name + '\n' +
+            'last name: ' + user.last_name);
+    } else {
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
+});
+};
+
+var getallthethings = function() {
+Cloud.Objects.query({
+    classname: 'cars',
+    page: 1,
+    per_page: 10,
+  // "want everything, get rid of where claus"  where: {
+        //color: 'blue'
+    //}
+}, function (e) {
+    if (e.success) {
+        alert('Success:\n' +
+            'Count: ' + e.cars.length);
+        for (var i = 0; i < e.cars.length; i++) {
+            var car = e.cars[i];
+            alert('id: ' + cars.id + '\n' +
+                'make: ' + car.make + '\n' +
+                'color: ' + car.color + '\n' +
+                'year: ' + car.year + '\n' +
+                'created_at: ' + car.created_at);
+        }
+    } else {
+        alert('Error:\n' +
+            ((e.error && e.message) || JSON.stringify(e)));
+    }
+});
+};
+
+
+
+
 
 
 
